a61259fa60c953c5aabca258d787e2f09acbc7f9
eric_song <jamsya@naver.com>
2019-02-04 22:06:12 +0900

    패키지 파일 암호화, 해시 라이브러리 적용 완료

--- Source/PragonRPG/HashStringLibrary.cpp
+++ Source/PragonRPG/HashStringLibrary.cpp
@@ -1,11 +1,23 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "HashStringLibrary.h"
+#include "sha512.h"
 
 FString UHashStringLibrary::HashString(FString inputString)
 {
-	FString outputString = inputString + "123123";
+	FString outputString;
 
 	
+	//std:string to FString
+	std::string MyStdString(TCHAR_TO_UTF8(*inputString));
+	   
+	//std:string에 해싱을 적용해 변환한다.
+	MyStdString = sha512(MyStdString);
+	
+	//변환한 std string을 Fstring으로 변환
+	outputString = MyStdString.c_str();
+
+	//결과물 Fstring 반환
+
 	return outputString;
 }
--- Source/PragonRPG/HashStringLibrary.h
+++ Source/PragonRPG/HashStringLibrary.h
@@ -6,8 +6,10 @@
 #include "Kismet/BlueprintFunctionLibrary.h"
 #include "HashStringLibrary.generated.h"
 
+
 /**
  * 
+블루프린트 상에서 스트링에 해시를 적용하기 위한 함수 정의 - SHA 해싱 적용
  */
 UCLASS()
 class PRAGONRPG_API UHashStringLibrary : public UBlueprintFunctionLibrary
@@ -15,6 +17,6 @@ class PRAGONRPG_API UHashStringLibrary : public UBlueprintFunctionLibrary
 	GENERATED_BODY()
 
 
-		UFUNCTION(Blueprintcallable )
+		UFUNCTION(Blueprintcallable )			   
 		static FString HashString(FString inputString);
 };
