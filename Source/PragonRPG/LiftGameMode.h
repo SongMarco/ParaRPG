4658d1bdf759d38305110c658181371ea3dd1992
eric_song <jamsya@naver.com>
2019-02-08 05:09:04 +0900

    게임리프트 모드 생성

---
+++ Source/PragonRPG/LiftGameMode.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Engine.h"
+#include "EngineGlobals.h"
+#include "GameLiftServerSDK.h"
+#include "LiftGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PRAGONRPG_API ALiftGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ALiftGameMode();
+	
+};
