c875ae9d8c826521f3d5e5ea0b4326387039bdba
eric_song <jamsya@naver.com>
2019-02-12 16:40:55 +0900

    성공적으로 ThirdParty 폴더 임포트함

--- Source/PragonRPG/GameLiftLibrary.cpp
+++ Source/PragonRPG/GameLiftLibrary.cpp
@@ -2,8 +2,7 @@
 #include "GameLiftLibrary.h"
 #include "Engine.h"
 #include "GameLiftServerSDK.h"
-//#include <MyLiftPlugin.h>
-
+#include <aws/gamelift/GameLiftClient.h>
 
 
 
--- Source/PragonRPG/PragonRPG.Build.cs
+++ Source/PragonRPG/PragonRPG.Build.cs
@@ -1,22 +1,31 @@
 // Fill out your copyright notice in the Description page of Project Settings.
-
+using System.IO;
 using UnrealBuildTool;
 
 public class PragonRPG : ModuleRules
 {
-	public PragonRPG(ReadOnlyTargetRules Target) : base(Target)
-	{
-		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "JsonUtilities", "Http", "Json", "GameLiftServerSDK"/*, "GameLiftClientSDK"*/ });
+    //convinience properties
+    private string ModulePath
+    {
+        get { return ModuleDirectory; }
+    }
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+    private string ThirdPartyPath
+    {
+        get { return Path.GetFullPath(Path.Combine(ModulePath, "../../ThirdParty/")); }
+    }
 
+    public PragonRPG(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "JsonUtilities", "Http", "Json", "GameLiftServerSDK"/*, "GameLiftClientSDK"*/ });
+
+        PrivateDependencyModuleNames.AddRange(new string[] { });
+                
+        LoadYJMagicLib(Target);
         //PublicIncludePaths.Add("D:/Ue4Project/ParaRPG/packages/vcpkg/packages/aws-sdk-cpp_x86-windows/include");
         //PublicAdditionalLibraries.Add("D:/Ue4Project/ParaRPG/packages/vcpkg/packages/aws-sdk-cpp_x86-windows/bin/aws-cpp-sdk-gamelift.dll");
-
-
 
         // Uncomment if you are using Slate UI
         // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
@@ -26,4 +35,44 @@ public class PragonRPG : ModuleRules
 
         // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
     }
+
+    public bool LoadYJMagicLib(ReadOnlyTargetRules Target)
+    {
+        bool isLibrarySupported = false;
+
+        if ((Target.Platform == UnrealTargetPlatform.Win64) || (Target.Platform == UnrealTargetPlatform.Win32))
+        {
+            isLibrarySupported = true;
+
+            string PlatformString = (Target.Platform == UnrealTargetPlatform.Win64) ? "x64" : "x86";
+            string LibrariesPath = Path.Combine(ThirdPartyPath, "GameLiftLib", "lib");
+
+            /*
+            test your path with:
+            using System; // Console.WriteLine("");
+            Console.WriteLine("... LibrariesPath -> " + LibrariesPath);
+            */
+
+            PublicAdditionalLibraries.Add(Path.Combine(LibrariesPath, "aws-cpp-sdk-gamelift." + PlatformString + ".lib"));
+        }
+
+        if (isLibrarySupported)
+        {
+            // Include path
+            PublicIncludePaths.Add(Path.Combine(ThirdPartyPath, "GameLiftLib", "Include"));
+        }
+
+        PublicDefinitions.Add(string.Format("WITH_YJ_MAGIC_LIB_BINDING={0}", isLibrarySupported ? 1 : 0));
+
+        return isLibrarySupported;
+    }
+
+
+  
+
+
+
+
+
+
 }
