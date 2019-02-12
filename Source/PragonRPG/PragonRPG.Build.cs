2967d841c05275ca4b6fd87e2e5fd8438f6f3aa4
eric_song <jamsya@naver.com>
2019-02-12 20:02:09 +0900

    절대 경로를 상대 경로로 수정함

--- Source/PragonRPG/PragonRPG.Build.cs
+++ Source/PragonRPG/PragonRPG.Build.cs
@@ -12,7 +12,7 @@ public class PragonRPG : ModuleRules
 
     private string ThirdPartyPath
     {
-        get { return Path.GetFullPath(Path.Combine(ModulePath, "../../ThirdParty/")); }
+        get { return Path.GetFullPath(Path.Combine(ModulePath, "../../packages/vcpkg/packages/")); }
     }
 
     public PragonRPG(ReadOnlyTargetRules Target) : base(Target)
@@ -47,7 +47,7 @@ public class PragonRPG : ModuleRules
             string PlatformString = (Target.Platform == UnrealTargetPlatform.Win64) ? "x64" : "x86";
 
             //D:\Ue4Project\ParaRPG\ThirdParty\GameLiftLib\lib
-            string LibrariesPath = Path.Combine(ThirdPartyPath, "GameLiftLib", "lib");
+            string LibrariesPath = Path.Combine(ThirdPartyPath, "aws-sdk-cpp_x64-windows", "lib");
 
             /*
             test your path with:
@@ -56,15 +56,14 @@ public class PragonRPG : ModuleRules
             */
 
             ////라이브러리(x.lib) 패스 추가 - 헤더 파일 추가 확인할 것(아래 If~내용)
+
             //PublicAdditionalLibraries.Add(Path.Combine(LibrariesPath, "aws-cpp-sdk-gamelift." + PlatformString + ".lib"));
 
-            //PublicAdditionalLibraries.Add("D:/Ue4Project/ParaRPG/ThirdParty/GameLiftLib/lib/aws-cpp-sdk-core.x64.lib");
-
-            PublicAdditionalLibraries.Add("D:/Ue4Project/ParaRPG/packages/vcpkg/packages/aws-sdk-cpp_x64-windows/lib/aws-cpp-sdk-core.lib");
-            PublicAdditionalLibraries.Add("D:/Ue4Project/ParaRPG/packages/vcpkg/packages/aws-sdk-cpp_x64-windows/lib/aws-cpp-sdk-gamelift.lib");
-            PublicAdditionalLibraries.Add("D:/Ue4Project/ParaRPG/packages/vcpkg/packages/aws-sdk-cpp_x64-windows/lib/aws-cpp-sdk-kinesis.lib");
-            PublicAdditionalLibraries.Add("D:/Ue4Project/ParaRPG/packages/vcpkg/packages/aws-sdk-cpp_x64-windows/lib/aws-cpp-sdk-s3.lib");
-     
+            PublicAdditionalLibraries.Add(Path.Combine(LibrariesPath, "aws-cpp-sdk-core" + ".lib"));
+            PublicAdditionalLibraries.Add(Path.Combine(LibrariesPath, "aws-cpp-sdk-gamelift" + ".lib"));
+            PublicAdditionalLibraries.Add(Path.Combine(LibrariesPath, "aws-cpp-sdk-kinesis" + ".lib"));
+            PublicAdditionalLibraries.Add(Path.Combine(LibrariesPath, "aws-cpp-sdk-s3" + ".lib"));
+                 
         }
 
         if (isLibrarySupported)
@@ -73,6 +72,7 @@ public class PragonRPG : ModuleRules
 
             // Include path for gamelift
             //PublicIncludePaths.Add(Path.Combine(ThirdPartyPath, "GameLiftLib", "Include"));
+            PublicIncludePaths.Add(Path.Combine(ThirdPartyPath, "aws-sdk-cpp_x64-windows", "Include"));
 
             //// Include path for aws core
             //PublicIncludePaths.Add(Path.Combine(ThirdPartyPath, "aws-cpp-sdk-core", "Include"));
@@ -81,7 +81,7 @@ public class PragonRPG : ModuleRules
             //PublicIncludePaths.Add("D:/Ue4Project/ParaRPG/ThirdParty/aws-cpp-sdk-core/include");
 
             // Include path for aws core
-            PublicIncludePaths.Add("D:/Ue4Project/ParaRPG/packages/vcpkg/packages/aws-sdk-cpp_x64-windows/include");
+            //PublicIncludePaths.Add("D:/Ue4Project/ParaRPG/packages/vcpkg/packages/aws-sdk-cpp_x64-windows/include");
         }
 
         PublicDefinitions.Add(string.Format("WITH_YJ_MAGIC_LIB_BINDING={0}", isLibrarySupported ? 1 : 0));
