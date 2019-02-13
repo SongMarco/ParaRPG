e3a98c232fd2d6bb39f1893c15709ab8cd74f430
eric_song <jamsya@naver.com>
2019-02-13 21:14:14 +0900

    빌드 모듈에 AWSCore를 추가하여 빌드 성공! 에디터도 잘 실행됨

--- Source/PragonRPG/PragonRPG.Build.cs
+++ Source/PragonRPG/PragonRPG.Build.cs
@@ -19,21 +19,16 @@ public class PragonRPG : ModuleRules
     {
         PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "JsonUtilities", "Http", "Json", "GameLiftServerSDK"/*, "GameLiftClientSDK"*/ });
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "JsonUtilities", "Http", "Json", "GameLiftServerSDK", "GameLiftClientSDK", "AWSCore" });
 
         PrivateDependencyModuleNames.AddRange(new string[] { });
 
-        LoadYJMagicLib(Target);
-        //PublicIncludePaths.Add("D:/Ue4Project/ParaRPG/packages/vcpkg/packages/aws-sdk-cpp_x86-windows/include");
-        //PublicAdditionalLibraries.Add("D:/Ue4Project/ParaRPG/packages/vcpkg/packages/aws-sdk-cpp_x86-windows/bin/aws-cpp-sdk-gamelift.dll");
 
-        // Uncomment if you are using Slate UI
-        // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 
-        // Uncomment if you are using online features
-        // PrivateDependencyModuleNames.Add("OnlineSubsystem");
+                          
 
-        // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+        //LoadYJMagicLib(Target);
+   
     }
 
     public bool LoadYJMagicLib(ReadOnlyTargetRules Target)
